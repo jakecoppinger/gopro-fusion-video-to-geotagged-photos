3759e44498771271a888afb5d4e2dd5a38cdbbef
Jake Coppinger <jake@jakecoppinger.com>
2022-12-15 22:28:42 +1100

    Fix geotagging

--- script.sh
+++ script.sh
@@ -2,20 +2,19 @@
 set +x
 set -e
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Add filename as first arg'
+if [[ $# -ne 1 ]] ; then
+    echo 'Pass front filenames'
     exit 0
 fi
 
-echo "Making frames"
+echo "Making folders"
 rm -rf frames/
 mkdir frames/
 
 rate=1
 
-echo "Generating frames..."
+echo "Generating front frames..."
 ffmpeg -i "$1"  -r $rate -q:v 2  frames/img%d.jpg
-
 
 echo "Extracting GPS..."
 # Deffs works with OG front image
@@ -26,11 +25,16 @@ exiftool -G -a "$1" > metadata.txt
 
 echo "Add timestamps to frames..."
 
-
 # MANUAL: Grab value of Track Create Date  in metadata
 echo "Setting original date time..."
-exiftool -datetimeoriginal="2022:12:15 18:29:38" frames/
+exiftool -datetimeoriginal="2022-12-15T08:23:08.654Z" frames/
 echo "Setting offsetted date time..."
 
 exiftool -fileorder FileName -ext jpg '-datetimeoriginal+<0:0:${filesequence;$_*=1}' frames/
+
+echo 'Geotag photos (adjusting +11 for Sydney time right now)'
+exiftool -ext jpg -geosync=+11:0:0 -geotag gps.gpx frames/
+
+echo "Removing .jpg_original..."
+rm -r frames/*.jpg_original
 
