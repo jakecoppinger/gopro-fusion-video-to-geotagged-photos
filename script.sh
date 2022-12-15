bd9c2bc2482d5fc1206dc7689d1b8c32f0409a0a
Jake Coppinger <jake@jakecoppinger.com>
2022-12-15 22:35:37 +1100

    Set extra meta

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 set +x
 set -e
 
-if [[ $# -ne 1 ]] ; then
-    echo 'Pass front filenames'
+if [[ $# -ne 2 ]] ; then
+    echo 'Pass front filename then equirectangular filename'
     exit 0
 fi
 
@@ -13,8 +13,8 @@ mkdir frames/
 
 rate=1
 
-echo "Generating front frames..."
-ffmpeg -i "$1"  -r $rate -q:v 2  frames/img%d.jpg
+echo "Generating frames..."
+ffmpeg -i "$2"  -r $rate -q:v 2  frames/img%d.jpg
 
 echo "Extracting GPS..."
 # Deffs works with OG front image
@@ -25,7 +25,7 @@ exiftool -G -a "$1" > metadata.txt
 
 echo "Add timestamps to frames..."
 
-# MANUAL: Grab value of Track Create Date  in metadata
+# CURRENTLY MANUAL: Grab value of first GPS coord in gps.gpx
 echo "Setting original date time..."
 exiftool -datetimeoriginal="2022-12-15T08:23:08.654Z" frames/
 echo "Setting offsetted date time..."
@@ -34,6 +34,10 @@ exiftool -fileorder FileName -ext jpg '-datetimeoriginal+<0:0:${filesequence;$_*
 
 echo 'Geotag photos (adjusting +11 for Sydney time right now)'
 exiftool -ext jpg -geosync=+11:0:0 -geotag gps.gpx frames/
+
+echo "Setting extra metadata..."
+exiftool -make="GoPro" -model=Fusion -ProjectionType=equirectangular -UsePanoramaViewer=True -CroppedAreaImageWidthPixels=3840 -CroppedAreaImageHeightPixels=1920 -FullPanoWidthPixels=3840 -FullPanoHeightPixels=1920 -CroppedAreaLeftPixels=0 -CroppedAreaTopPixels=0 
+
 
 echo "Removing .jpg_original..."
 rm -r frames/*.jpg_original
